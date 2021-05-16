62546d793d86072eebd8c80097f6ba83779948be
Adrian-Batista <62628581+Adrian-Batista@users.noreply.github.com>
2021-05-16 15:05:26 -0300

    Update 103.py

--- 103.py
+++ 103.py
@@ -82,10 +82,25 @@ print(new_cheese_str)
 # 3) Quantos caracteres cada título tem?
 # 4) Imprima com a formatacao: {Titulo} - {Autor}, {Ano}
 
-
 book1 = 'Homo Deus by Yuval Noah Harari, 2015'
 book2 = 'Antifragile by Nassim Nicholas Taleb, 2012'
 book3 = 'Fooled by Randomness by Nassim Nicholas Taleb, 2001'
+
+lista = book1.split('by')
+resposta1 = "".join(lista[0:-1:])
+resposta1 = resposta1.strip()
+len(resposta1)
+
+lista = book2.split('by')
+resposta2 = "".join(lista[0:-1:])
+resposta2 = resposta2.strip()
+len(resposta2)
+
+lista = book3.split('by')
+resposta3 = "".join(lista[0:-1:])
+resposta3 = resposta3.strip()
+len(resposta3)
+
 
 # 5) Verifique se uma palavra é uma palindrome perfeita.
 # Palindrome perfeito sao palavras que ao serem escritas em ordem reversa,
@@ -96,3 +111,16 @@ palindrome_one = 'ovo'
 palindrome_two = 'Natan'
 palindrome_three = 'luz azul'
 palindrome_four = 'caneta azul'
+
+
+verifica = palindrome_one.replace(" ", "").lower()
+verifica == verifica[::-1]
+
+verifica = palindrome_two.replace(" ", "").lower()
+verifica == verifica[::-1]
+
+verifica = palindrome_three.replace(" ", "").lower()
+verifica == verifica[::-1]
+
+verifica = palindrome_four.replace(" ", "").lower()
+verifica == verifica[::-1]

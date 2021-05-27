17929043237eb88f1dd5ef4f5928953d27d6bdeb
Adrian-Batista <62628581+Adrian-Batista@users.noreply.github.com>
2021-05-27 20:34:17 -0300

    Update 104.py

--- 104.py
+++ 104.py
@@ -20,16 +20,18 @@ print(funcao(lista1,lista2))
 
 print("------------------------")
 # 2) Crie uma funcao que verifica se duas strings são palindromes perfeitas. Faca as 'limpeza'/sanitizacao necessarias.  Retorne True ou False.
-valor = 'Luz azul'
+string1 = 'Luz azul'
+string2 = 'Luzazul'
 
-def funcao2(valor):
-    verifica = valor.replace(" ", "").lower()
-    if verifica == verifica[::-1]:
-        print('A palavra: ' + verifica + ' é uma palindrome!')
+def funcao2(string1, string2):
+    string1 = string1.replace(" ", "").lower()
+    string2 = string2.replace(" ", "").lower()
+    if string1 == string2[::-1]:
+        print('A palavra: ' + str(string2) + ' é uma palindrome perfeita de: ' + str(string1))
         return True
     else:
-        print('A palavra: ' + verifica + ' NÂO é uma palindrome!')
+        print('A palavra: ' + str(string2) + ' NÂO é uma palindrome de: ' + str(string1))
         return False
 
-print(funcao2(valor))
+print(funcao2(string1, string2))
 # OBS.: Utilize apenas o que foi estudado ate agora
